4197a610598439ce50354894900aa4014a93fc37
Vaibhav Rathore <69416365+vaibhavR11@users.noreply.github.com>
2024-05-25 17:16:22 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+alert( 3 + 3) // we are using nodejs, not browser 
+
+let name = "vaibhav"
+let age = 18
+let isLoggedIn = false
+let state; // undefined
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
